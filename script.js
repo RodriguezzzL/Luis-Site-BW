1e9e8f6a3ec103d1be44eda1652476961b1c1159
RodriguezzzL <rodriguezl972@outlook.com>
2024-12-12 18:16:08 -0800

    palindrom side project completed with styling.

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ function randomColor() {
 
   body.style.background = color;
 }
+
+const input = document.getElementById("input");
+
+function check() {
+  const value = input.value;
+
+  const reverse = reverseString(value);
+
+  if (value === reverse) {
+    alert("Palindrom detected");
+  } else {
+    alert("no palindrom detected");
+  }
+}
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
