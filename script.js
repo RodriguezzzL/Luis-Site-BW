f0a0b00f2861959071740237f389d1c58a69fed6
RodriguezzzL <rodriguezl972@outlook.com>
2024-12-14 14:22:31 -0800

    stop watch project completed

--- script.js
+++ script.js
@@ -73,3 +73,37 @@ function generateQuote() {
     break;
   }
 }
+
+let secondsElapsed = 0;
+let minutesElapsed = 0;
+let interval = null;
+
+const time = document.getElementById("time");
+
+function padStart(value) {
+  return String(value).padStart(2, "0");
+}
+
+function timer() {
+  secondsElapsed++;
+  setTime();
+}
+function setTime() {
+  const minutes = Math.floor(secondsElapsed / 60);
+  const seconds = secondsElapsed % 60;
+  time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}`;
+}
+function startClock() {
+  if (interval) stopClock();
+  interval = setInterval(timer, 1000);
+}
+
+function stopClock() {
+  clearInterval(interval);
+}
+
+function resetClock() {
+  stopClock();
+  secondsElapsed = 0;
+  setTime();
+}
